a879207d29638dea1f92b6dee7ced59af1dce6a2
ned-elcu <ned3d@yahoo.com>
2025-04-30 12:48:47 +0300

    Update firebase-config.js

--- chat/js/firebase-config.js
+++ chat/js/firebase-config.js
@@ -3,10 +3,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyBJyiyELkxcrUx_YYz010qc11ldLXs1Iwg",
   authDomain: "pls-chat.firebaseapp.com",
   projectId: "pls-chat",
-  storageBucket: "pls-chat.firebasestorage.app",
   messagingSenderId: "257223618239",
   appId: "1:257223618239:web:802d4c3320c4fceb07200a",
-  measurementId: "G-1N5R5HFQ01"
+  measurementId: "G-1N5R5HFQ01",
+  databaseURL: "https://pls-chat-default-rtdb.firebaseio.com" // Added for connection monitoring
 };
 
 // Initialize Firebase
