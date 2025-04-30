39c98bca9156c87fb7bf30a49063b48cf7cf9642
ned-elcu <ned3d@yahoo.com>
2025-04-30 11:48:31 +0300

    Create firebase-config.js

---
+++ chat/js/firebase-config.js
@@ -0,0 +1,18 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJyiyELkxcrUx_YYz010qc11ldLXs1Iwg",
+  authDomain: "pls-chat.firebaseapp.com",
+  projectId: "pls-chat",
+  storageBucket: "pls-chat.firebasestorage.app",
+  messagingSenderId: "257223618239",
+  appId: "1:257223618239:web:802d4c3320c4fceb07200a",
+  measurementId: "G-1N5R5HFQ01"
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
