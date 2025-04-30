ef9b679b85f56de774b909066bf4245a7c992ce6
ned-elcu <ned3d@yahoo.com>
2025-04-30 12:05:24 +0300

    Update firebase-config.js

--- chat/js/firebase-config.js
+++ chat/js/firebase-config.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase-config.js should be:
 const firebaseConfig = {
   apiKey: "AIzaSyBJyiyELkxcrUx_YYz010qc11ldLXs1Iwg",
   authDomain: "pls-chat.firebaseapp.com",
@@ -17,5 +10,4 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
